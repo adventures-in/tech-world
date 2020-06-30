448383b447b2fbe30c06794c47afd218a28e32c3
Nick M <nick.meinhold@gmail.com>
2020-06-30 21:57:55 +1000

    Add redirect action and button to auth page

--- lib/widgets/auth/auth_page.dart
+++ lib/widgets/auth/auth_page.dart
@@ -5,8 +5,45 @@ class AuthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Center(
-        child: Text('Auth'),
-      ),
+          child: Container(
+              width: 210,
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  onPressed: () {
+                    print('Button Clicked.');
+                  },
+                  textColor: Colors.white,
+                  color: Colors.pink,
+                  padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            color: Colors.pink,
+                            padding: EdgeInsets.fromLTRB(10, 4, 4, 4),
+                            child: Text(
+                              'Button With Right Icon',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(4, 0, 10, 0),
+                            child: Icon(
+                              Icons.backup,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                        ],
+                      ))))
+          // MaterialButton(
+          //   child: Text('Sign in with GitHub'),
+          //   onPressed: () {},
+          // ),
+          ),
     );
   }
 }
