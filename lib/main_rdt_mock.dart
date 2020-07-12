f81dd0da874046e58f55652bbf18e20008288fe6
Nick M <nick.meinhold@gmail.com>
2020-07-13 03:20:13 +1000

    Added separate entry point file that uses mocks

---
+++ lib/main_rdt_mock.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:adventures_in_tech_world/middleware/app_middleware.dart';
+import 'package:adventures_in_tech_world/models/app/app_state.dart';
+import 'package:adventures_in_tech_world/reducers/app_reducer.dart';
+import 'package:adventures_in_tech_world/services/git_hub_service.dart';
+import 'package:adventures_in_tech_world/services/mocks/mock_auth_service.dart';
+import 'package:adventures_in_tech_world/services/mocks/mock_database_service.dart';
+import 'package:adventures_in_tech_world/services/navigation_service.dart';
+import 'package:adventures_in_tech_world/services/platform_service.dart';
+import 'package:adventures_in_tech_world/widgets/adventures_in_app.dart';
+import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_remote_devtools/redux_remote_devtools.dart';
+
+void main() async {
+  final remoteDevtools = RemoteDevToolsMiddleware('localhost:8000');
+
+  /// we use a [GlobalKey] to allow navigation from a service
+  final navKey = GlobalKey<NavigatorState>();
+
+  /// Create services
+  final authService = MockAuthService();
+  final databaseService = MockDatabaseService();
+  final platformService = PlatformService();
+  final navigationService = NavigationService(navKey);
+  final gitHubService = GitHubService();
+
+  /// Create the redux store
+  final store =
+      Store<AppState>(appReducer, initialState: AppState.init(), middleware: [
+    remoteDevtools,
+    ...createAppMiddleware(
+        authService: authService,
+        gitHubService: gitHubService,
+        databaseService: databaseService,
+        platformService: platformService,
+        navigationService: navigationService)
+  ]);
+
+  // give RDT access to the store
+  remoteDevtools.store = store;
+
+  // try to connect and print out any exception thrown
+  try {
+    await remoteDevtools.connect();
+  } on SocketException catch (e) {
+    print(e);
+  }
+
+  runApp(AdventuresInApp(store, navKey));
+}
