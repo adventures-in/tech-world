c05d4e1e7f5cfd04f88c6c6a2fcd7efc308f855c
Nick M <nick.meinhold@gmail.com>
2020-07-18 03:12:59 +1000

    Add cloud function for batch deleting users
    - updated firebase-admin for deleteUsers function
    - added a cloud function that triggers when a doc is added at /delete
    and deletes all users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,9 +4,13 @@ admin.initializeApp();
 
 import { saveDetails } from './auth/on_first_sign_in';
 import { exchangeCodeWithGitHub } from './auth/exchange_code_for_token';
+import { deleteUsers } from './auth/on_delete_users_entry';
 
 // when a new account is created, add auth details to the database
 export const saveDetailsOnFirstSignIn = functions.auth.user().onCreate(saveDetails);
 
 // endpoint for exchanging code from github and a secret for an auth token 
 export const getAuthTokenFromGitHub = functions.https.onRequest(exchangeCodeWithGitHub);
+
+// delete users in bulk, should be taken out when in production 
+export const deleteUsersOnDeleteUsersEntry = functions.firestore.document('delete/{docId}').onCreate(deleteUsers);
